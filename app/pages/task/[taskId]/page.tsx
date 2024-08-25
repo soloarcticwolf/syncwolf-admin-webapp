44a83883cb32863e58fd543b6a68d4563e70ea5b
Sanwar Hussain <work.sanwar.hussain@gmail.com>
2024-08-25 16:01:15 +0530

    Readme update

--- app/pages/task/[taskId]/page.tsx
+++ app/pages/task/[taskId]/page.tsx
@@ -1,6 +1,7 @@
+import { PageProps } from '@/.next/types/app/layout'
 import { NextPage } from 'next'
 
-const TaskInfo: NextPage = (props) => {
+const TaskInfo: NextPage = (props: PageProps) => {
 	console.log('**********taskinfo', props)
 	return <div>{props.params.taskId}</div>
 }
